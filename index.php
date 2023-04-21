49c1f1c05e074f6a6c0a1ff82e1e99b0cad92d1b
Anur Peljto <anur.peljto@stu.ibu.edu.ba>
2023-04-21 16:19:08 +0200

    upd

--- index.php
+++ index.php
@@ -4,8 +4,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS, PATCH');
 require 'vendor/autoload.php';
 
-Flight::register('db', 'PDO', 
-array('mysql:host=localhost;dbname=lab3_db','root',''));
+Flight::register('db', 'PDO', array('mysql:host=localhost;dbname=lab3_db','root',''));
 
 Flight::route('GET /api/users', function(){
     $users = Flight::db()->query('SELECT * FROM Users', PDO::FETCH_ASSOC)->fetchAll();
@@ -13,6 +12,6 @@ Flight::route('GET /api/users', function(){
     Flight::json($users);
     });
 
-    
+
  Flight::start();
 ?>
