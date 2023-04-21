85d0a15d60e039729e92e205584aa2734da41a81
Anur Peljto <anur.peljto@stu.ibu.edu.ba>
2023-04-21 16:09:35 +0200

    troubleshooting

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php 
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS, PATCH');
+
 require 'vendor/autoload.php';
 
 Flight::register('db', 'PDO', 
