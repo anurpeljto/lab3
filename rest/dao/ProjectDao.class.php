d264c3f0bdd652417e0759f8a712b2c187fab4b1
anurpeljto <anur.peljto@stu.ibu.edu.ba>
2023-04-21 19:35:50 +0200

    updated

---
+++ rest/dao/ProjectDao.class.php
@@ -0,0 +1,22 @@
+<?php
+
+class UserDao {
+    private $conn;
+    private $username = 'root';
+    private $host = 'localhost';
+    private $password = 'root';
+    private $database = 'lab3';
+
+    public function __construct(){
+        try {
+            $conn = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+            $conn ->setAttribute(PDO::AFTER_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+        }
+        catch(PDOException $exc){
+            echo "Connection failed: " . $exc->getMessage();
+        }
+    
+}
+}
+?>
